240d307db74efc94c0f367a8cdaa06f7f8e7f905
silently0801 <silently0801@gmail.com>
2020-04-07 21:43:29 +0800

    2020-04-07-end

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
     if(window.innerWidth >1279)
-    $(".mask").height(parseInt($("html").height()) - parseInt($("footer").height()));
+        $(".mask").height(parseInt($("html").height()) - parseInt($("footer").height()));
 });
 $(window).resize(function () {
     if(window.innerWidth >1279)
