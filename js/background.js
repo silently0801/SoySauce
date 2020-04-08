75b756014baf843185d22fb7baed451d617a1ec4
silently0801 <silently0801@gmail.com>
2020-04-08 18:45:31 +0800

    2020-04-08

--- js/background.js
+++ js/background.js
@@ -1,9 +1,13 @@
 
 $(document).ready(function () {
-    if(window.innerWidth >1279)
+    if (window.innerWidth > 1279)
         $(".mask").height(parseInt($("html").height()) - parseInt($("footer").height()));
 });
 $(window).resize(function () {
-    if(window.innerWidth >1279)
+    if (window.innerWidth > 1279)
+        $(".mask").height(parseInt($("html").height()) - parseInt($("footer").height()));
+});
+$(window).scroll(function () {
+    if (window.innerWidth > 1279)
         $(".mask").height(parseInt($("html").height()) - parseInt($("footer").height()));
 });
