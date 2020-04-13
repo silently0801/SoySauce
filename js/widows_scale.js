82b7b7e0a144ed081c325b93ca04b8af17e003bc
silently0801 <silently0801@gmail.com>
2020-04-13 20:37:13 +0800

    2020-04-13

--- js/widows_scale.js
+++ js/widows_scale.js
@@ -1,10 +1,36 @@
-$(document).ready(function () {
-    let scaleValue = (1 / window.devicePixelRatio);
-    if (window.innerWidth > 1279)
-        $("html").css({ "zoom": scaleValue });
-});
-$(window).resize(function () {
-    let scaleValue = (1 / window.devicePixelRatio);
-    if (window.innerWidth > 1279)
-        $("html").css({ "zoom": scaleValue });
-});
+// $(document).ready(function () {
+//     var system = {
+//         win: false,
+//         mac: false,
+//         xll: false,
+//         ipad: false
+//     };   
+//     var p = navigator.platform;
+//     system.win = p.indexOf("Win") == 0;
+//     system.mac = p.indexOf("Mac") == 0;
+//     system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+//     system.ipad = (navigator.userAgent.match(/iPad/i) != null) ? true : false;
+//     let scaleValue = (1 / window.devicePixelRatio);
+//     console.log(p);
+    
+//     if (system.win || system.mac || system.xll)
+//         $("html").css({ "zoom": scaleValue });
+// });
+// $(window).resize(function () {
+//     var system = {
+//         win: false,
+//         mac: false,
+//         xll: false,
+//         ipad: false
+//     };   
+//     var p = navigator.platform;
+//     system.win = p.indexOf("Win") == 0;
+//     system.mac = p.indexOf("Mac") == 0;
+//     system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
+//     system.ipad = (navigator.userAgent.match(/iPad/i) != null) ? true : false;
+//     let scaleValue = (1 / window.devicePixelRatio);
+//     console.log(p);
+    
+//     if (system.win || system.mac || system.xll)
+//         $("html").css({ "zoom": scaleValue });
+// });
